d98ad92b9ea41adf14476c7e2ce3f1f45be16db6
657994434 <657994434@qq.com>
2016-10-24 23:17:46 +0800

    图灵机器人自动回复

--- src/main/java/com/wechat/connect/controller/ConnectWeChat.java
+++ src/main/java/com/wechat/connect/controller/ConnectWeChat.java
@@ -3,10 +3,12 @@ package com.wechat.connect.controller;
 import com.thoughtworks.xstream.XStream;
 import com.wechat.Utils.SerializeXmlUtil;
 import com.wechat.Utils.common.SHA1;
+import com.wechat.base.JuheRobor;
 import com.wechat.connect.domain.ImageMessage;
 import com.wechat.connect.domain.InputMessage;
 import com.wechat.connect.domain.MsgType;
 import com.wechat.connect.domain.OutputMessage;
+import net.sf.json.JSONObject;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -30,7 +32,7 @@ public class ConnectWeChat {
     @RequestMapping(value = "weChatReply", method = {RequestMethod.GET, RequestMethod.POST}, produces = "application/json; charset=utf-8")
     public
     @ResponseBody
-    void chat(HttpServletRequest request, HttpServletResponse response) throws Exception{
+    void chat(HttpServletRequest request, HttpServletResponse response) throws Exception {
         request.setCharacterEncoding("utf-8");
         response.setCharacterEncoding("utf-8");
         boolean isGet = request.getMethod().toLowerCase().equals("get");
@@ -115,18 +117,20 @@ public class ConnectWeChat {
         Long returnTime = Calendar.getInstance().getTimeInMillis() / 1000;// 返回时间
 
         // 取得消息类型
-            String msgType = inputMsg.getMsgType();
-            // 根据消息类型获取对应的消息内容
-            if (msgType.equals(MsgType.Text.toString())) {
-                StringBuffer str = new StringBuffer();
-                str.append("<xml>");
-                str.append("<ToUserName><![CDATA[" + custermName + "]]></ToUserName>");
-                str.append("<FromUserName><![CDATA[" + serverName + "]]></FromUserName>");
-                str.append("<CreateTime>" + returnTime + "</CreateTime>");
-                str.append("<MsgType><![CDATA[" + msgType + "]]></MsgType>");
-                str.append("<Content><![CDATA[你说的是：" + inputMsg.getContent() + "，吗？]]></Content>");
-                str.append("</xml>");
-                response.getWriter().write(str.toString());
+        String msgType = inputMsg.getMsgType();
+        // 根据消息类型获取对应的消息内容
+        if (msgType.equals(MsgType.Text.toString())) {
+            String resultStr = JuheRobor.getAnswerRequest(inputMsg.getContent());
+            StringBuffer str = new StringBuffer();
+            str.append("<xml>");
+            str.append("<ToUserName><![CDATA[" + custermName + "]]></ToUserName>");
+            str.append("<FromUserName><![CDATA[" + serverName + "]]></FromUserName>");
+            str.append("<CreateTime>" + returnTime + "</CreateTime>");
+            str.append("<MsgType><![CDATA[" + msgType + "]]></MsgType>");
+//                str.append("<Content><![CDATA[你说的是：" + inputMsg.getContent() + "，吗？]]></Content>");
+            str.append("<Content><![CDATA[" + resultStr + "]]></Content>");
+            str.append("</xml>");
+            response.getWriter().write(str.toString());
         }
         // 获取并返回多图片消息
         if (msgType.equals(MsgType.Image.toString())) {
